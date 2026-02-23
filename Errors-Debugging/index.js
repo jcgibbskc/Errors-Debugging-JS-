fd66dfa3f85bad658b096a063d00076ea6d25d0a
JC Gibbs <jgibbs@icstars.org>
2026-02-23 14:09:07 -0600

    Second function findMax

--- Errors-Debugging/index.js
+++ Errors-Debugging/index.js
@@ -8,8 +8,21 @@ function calculateTotal(numbers) {
  return total;
 }
 
+
+function findMax(numbers) {
+  let max = numbers[0]; // start by assuming the first number is the largest
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i]; // update max if a bigger number is found
+    }
+  }
+  return max;
+}
 const data1 = [1, 2, 3];
 const data2 = [4, 5, 6];
 
-console.log(calculateTotal(data1));
-console.log(calculateTotal(data2));
+console.log(calculateTotal(data1));//6
+console.log(calculateTotal(data2)); //15
+
+console.log(findMax(data1)); //3
+console.log(findMax(data2)); //6
