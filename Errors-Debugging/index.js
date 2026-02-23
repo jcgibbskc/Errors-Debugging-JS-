75935edc3db5540cee5363700ccac2081220dab7
JC Gibbs <jgibbs@icstars.org>
2026-02-23 14:29:23 -0600

    Fixed error message.  Moved return max inside the closing brace

--- Errors-Debugging/index.js
+++ Errors-Debugging/index.js
@@ -1,29 +1,26 @@
-// STARTER CODE
-
 function calculateTotal(numbers) {
- let total = 0;
- for (let i = 0; i < numbers.length; i++) { // BUG: <= causes undefined
-   total += numbers[i];
- }
- return total;
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
+  return total;
 }
-
 
 function findMax(numbers) {
-  let max = numbers[0]; // start by assuming the first number is the largest
-  for (let i = 1; i < number.length; i++) {
+  let max = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
     if (numbers[i] > max) {
-      max = numbers[i]; // update max if a bigger number is found
+      max = numbers[i];
     }
   }
+  return max; // FIXED: moved inside the closing brace
 }
-  return max;
 
 const data1 = [1, 2, 3];
 const data2 = [4, 5, 6];
 
-console.log(calculateTotal(data1));//6
-console.log(calculateTotal(data2)); //15
+console.log(calculateTotal(data1)); // 6
+console.log(calculateTotal(data2)); // 15
 
-console.log(findMax(data1)); //3
-console.log(findMax(data2)); //6
+console.log(findMax(data1)); // 3
+console.log(findMax(data2)); // 6
